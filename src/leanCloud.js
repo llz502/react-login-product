8604376652761158c984657acc68c4bd6c510486
llz502 <1872132961@qq.com>
2017-10-31 23:11:39 +0800

    resetPretyCode

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -9,14 +9,16 @@ AV.init({
 
 export default AV
  
-export function signUp(username, password, successFn, errorFn){
+export function signUp(email, username, password, successFn, errorFn){
   // 新建 AVUser 对象实例
   var user = new AV.User()
   // 设置用户名
   user.setUsername(username)
   // 设置密码
   user.setPassword(password)
-  
+  // 设置邮箱
+  user.setEmail(email)
+
   user.signUp().then(function (loginedUser) {
     let user = getUserFromAVUser(loginedUser)
     successFn.call(null, user)
@@ -49,6 +51,16 @@ export function signOut(){
   AV.User.logOut()
   return undefined
 }
+
+export function sendPasswordResetEmail(email, successFn, errorFn){
+  AV.User.requestPasswordReset(email).then(function (success) {
+    successFn.call() 
+  }, function (error) {
+    console.dir(error)
+  })
+}
+
+
  function getUserFromAVUser(AVUser){
    return {
      id: AVUser.id,
